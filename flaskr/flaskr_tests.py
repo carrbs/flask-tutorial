f665c68af708c7d5ea8fa3bca01813e250689d60
Ben Carr <carrbens@gmail.com>
2016-09-19 14:41:19 -0700

    add login/logut tests and posting articles

--- flaskr/flaskr_tests.py
+++ flaskr/flaskr_tests.py
@@ -3,6 +3,7 @@ import flaskr
 import unittest
 import tempfile
 
+
 class FlaskrTestCase(unittest.TestCase):
 
     def setUp(self):
@@ -16,9 +17,38 @@ class FlaskrTestCase(unittest.TestCase):
         os.close(self.db_fd)
         os.unlink(flaskr.app.config['DATABASE'])
 
+    def login(self, username, password):
+        return self.app.post('/login', data=dict(
+            username=username,
+            password=password
+        ), follow_redirects=True)
+
+    def logout(self):
+        return self.app.get('/logout', follow_redirects=True)
+
     def test_empty_db(self):
         rv = self.app.get('/')
         assert b'No entries here so far' in rv.data
+
+    def test_login_logout(self):
+        rv = self.login('admin', 'default')
+        assert 'You were logged in' in rv.data
+        rv = self.logout()
+        assert 'You were logged out' in rv.data
+        rv = self.login('admen', 'default')
+        assert 'Invalid username' in rv.data
+        rv = self.login('admin', 'deefault')
+        assert 'Invalid password' in rv.data
+
+    def test_messages(self):
+        self.login('admin', 'default')
+        rv = self.app.post('/add', data=dict(
+            title='<Hello>',
+            text='<strong>HTML</strong> allowed here'
+        ), follow_redirects=True)
+        assert 'No entries here so far' not in rv.data
+        assert '&lt;Hello&gt;' in rv.data
+        assert '<strong>HTML</strong> allowed here' in rv.data
 
 if __name__ == '__main__':
     unittest.main()
